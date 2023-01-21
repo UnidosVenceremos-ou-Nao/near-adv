922256a66645e550fa8e3681fc0d41843c059d28
Igor <igoredm@gmail.com>
2023-01-21 15:45:18 -0300

    'Angular Fire'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './security/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 @NgModule({
   declarations: [AppComponent, LoginComponent],
   imports: [
@@ -17,6 +20,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ReactiveFormsModule,
     NgbModule,
     FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'near-adv',
+    appId: '1:540811149884:web:0e2539efb410dbcf087939',
+    storageBucket: 'near-adv.appspot.com',
+    apiKey: 'AIzaSyDVEpBEuTKz3R0o85ueU7Ob1-kdiHs6sTA',
+    authDomain: 'near-adv.firebaseapp.com',
+    messagingSenderId: '540811149884',
+    measurementId: 'G-0SF73ECBQ2',
+  },
+  production: false,
+};
