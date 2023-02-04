715165c993f64a5b389be511ff9db3aff674eb64
Paulo Mesquita <paulomesquita0@gmail.com>
2023-02-04 18:13:06 -0300

    Ajustes de inputs

--- src/app/shared/campos/inputdocumento/inputdocumento.component.ts
+++ src/app/shared/campos/inputdocumento/inputdocumento.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MascaraDocumento, TipoMascara } from './../../interfaces/campo';
 
 @Component({
   selector: 'near-documento',
@@ -6,15 +7,28 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./inputdocumento.component.scss'],
 })
 export class InputCpfCnpjComponent {
-  @Input('nome') nome: string = '';
-  @Input('valorInicial') valorInicial: any = '';
-  @Input('identificador') identificador: string = '';
-  @Input('desabilitar') desabilitar: any = false;
-  @Input('mascara') mascara: string = '';
+  @Input() nome: string = '';
+  @Input() valorInicial: any = '';
+  @Input() identificador: string = '';
+  @Input() desabilitar: any = false;
+  @Input() mascara: string = '000.000.000-00||00.000.000/0000-00';
+  @Input() tipo: string = '';
 
   @Output() valor = new EventEmitter();
 
+  ngOnInit() {
+    this.getTipoMascara();
+  }
+
   onChange(item: any) {
     this.valor.emit(item.target.value);
+  }
+
+  getTipoMascara() {
+    if (this.tipo.toUpperCase() == TipoMascara.CPF) {
+      this.mascara = MascaraDocumento.CPF;
+    } else if (this.tipo.toUpperCase() == TipoMascara.CPF) {
+      this.mascara = MascaraDocumento.CPF;
+    }
   }
 }
--- src/app/shared/interfaces/campo.ts
+++ src/app/shared/interfaces/campo.ts
@@ -7,6 +7,15 @@ export interface Campo {
   mascara?: string;
   classe?: string;
 }
+export enum MascaraDocumento {
+  CPF = '000.000.000-00',
+  CNPJ = '00.000.000/0000-00',
+}
+
+export enum TipoMascara {
+  CPF = 'CPF',
+  CNPJ = 'CNPJ',
+}
 
 type TipoInput =
   | 'daterange'
